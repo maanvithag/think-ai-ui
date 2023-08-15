823ce45a44383418ff29a35cb57625c95286c309
maanvithag <mgongall@iu.edu>
2023-08-15 13:01:56 -0400

    clean up

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="ThinkAI">
-      <div className="rounded-lg p-2 bg-gradient-to-r from-slate-500 to-slate-600">
+      <div className="rounded p-2 bg-gradient-to-r from-slate-500 to-slate-600">
         <h4 className="font-mono text-white ">
           thinkAi
         </h4>
